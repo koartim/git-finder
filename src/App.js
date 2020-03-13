12ba600392df634f531b0f0a319e399c4e3e4a01
Tim Koar <koartimothy@gmail.com>
2020-03-13 11:58:55 -0400

    adding search functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 
 class App extends React.Component {
 
@@ -23,6 +24,16 @@ class App extends React.Component {
     })
   }
 
+  searchUsers = (text) => {
+    this.state.users.map(user => {
+      if (user.login.includes(text)) {
+          console.log(user.login)
+      } else {
+        console.log("user not found")
+      }
+    })
+  }
+
 render() {
 
   const { users } =  this.state
@@ -30,6 +41,7 @@ render() {
     <div className="App">
       <NavBar title="GitHub" icon='fab fa-github'/>
       <div className="container">
+      <Search searchUsers={this.searchUsers} />
        <Users loading= {this.state.loading} users={users} />
       </div>
     
---
+++ src/components/users/Search.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Search extends React.Component {
+
+    state = {
+        text: ''
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.searchUsers(this.state.text)
+        this.setState({
+            text: ''
+        })
+    }
+ 
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.onSubmit} className="form">
+                    <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
+                    <input type="submit" value="Search" className="btn btn-dark btn-block" value='Search'/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search
