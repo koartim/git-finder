1fe77228b80282f2c3dd2bbfc440b5e31fca1d7d
Tim Koar <koartimothy@gmail.com>
2020-03-17 11:23:49 -0400

    added clear users button

--- src/App.js
+++ src/App.js
@@ -12,20 +12,21 @@ class App extends React.Component {
     loading: false
   }
 
-  // async componentDidMount() {
-  //   fetch(`https://api.github.com/users?client_id=
-  //   ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-  //   ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-  //   .then(rsp => rsp.json())
-  //   .then(data => {
-  //     this.setState({
-  //       users: data,
-  //       loading: false
-  //     })
-  //   })
-  // }
+  async componentDidMount() {
+    fetch(`https://api.github.com/users?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    .then(rsp => rsp.json())
+    .then(data => {
+      this.setState({
+        users: data,
+        loading: false
+      })
+    })
+  }
 
   searchUsers = async text => {
+    this.setState({loading: true})
     const rsp = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
@@ -34,16 +35,22 @@ class App extends React.Component {
 
   }
 
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    })
+  }
+
 render() {
-  console.log(this.state.users)
+  const { loading, users } = this.state
   return (
     <div className="App">
       <NavBar title="GitHub" icon='fab fa-github'/>
       <div className="container">
-      <Search searchUsers={this.searchUsers} />
-       <Users loading= {this.state.loading} users={this.state.users} />
+      <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} />
+       <Users loading= {loading} users={users} />
       </div>
-    
     </div>
     
   );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends React.Component {
 
     state = {
         text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.func.isRequired
     }
 
     onChange = (e) => {
@@ -19,16 +26,22 @@ class Search extends React.Component {
             text: ''
         })
     }
+
+
  
 
     render() {
-        console.log(this.props.searchUsers)
+        const {showClear, clearUsers} = this.props
         return(
             <div>
                 <form onSubmit={this.onSubmit} className="form">
                     <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
-                    <input type="submit" value="Search" className="btn btn-dark btn-block" value='Search'/>
+                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
+                {showClear && (
+                   <button class="btn btn-light btn-block" onClick={clearUsers}>Clear</button>     
+                )}
+                
             </div>
         )
     }
